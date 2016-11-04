cac4f588368da9a8f8e4047534de3109703ba6df
Jonic Linley <jonic@100yen.co.uk>
2016-11-04 17:57:28 +0000

    Tidied indentation, and lights now turn off when script exits

--- fire_effect.py
+++ fire_effect.py
@@ -1,17 +1,22 @@
+import blinkt
 from blinkt import set_brightness, set_pixel, show
 import random
 import time
 
+blinkt.set_clear_on_exit()
+
 colors = [
-        [255, 0, 0],    # red
-        [255, 255, 0],   # yellow
-        [255, 165, 0]   # orange
+    [255, 0,   0], # red
+    [255, 255, 0], # yellow
+    [255, 165, 0]  # orange
 ]
+
+interval = 0.05
 
 while True:
     brightness = random.random()
-    color = random.choice(colors)
-    pixel = random.randint(0, 7)
+    color      = random.choice(colors)
+    pixel      = random.randint(0, 7)
     
     r = color[0]
     g = color[1]
@@ -20,4 +25,5 @@ while True:
     set_brightness(brightness)
     set_pixel(pixel, r, g, b)
     show()
-    time.sleep(0.05)
+
+    time.sleep(interval)

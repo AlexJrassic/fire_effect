8e99210136f49b33bb7c1142b649db6c20b8deaa
Jonic Linley <jonic@100yen.co.uk>
2016-11-04 19:49:08 +0000

    Lowered brightness to prevent eye-scorching, and toned yellow down a little

--- fire_effect.py
+++ fire_effect.py
@@ -7,23 +7,24 @@ blinkt.set_clear_on_exit()
 
 colors = [
     [255, 0,   0], # red
-    [255, 255, 0], # yellow
+    [255, 194, 0], # amber
     [255, 165, 0]  # orange
 ]
 
 interval = 0.05
 
 while True:
-    brightness = random.random()
+    brightness = random.uniform(0, 0.2)
     color      = random.choice(colors)
     pixel      = random.randint(0, 7)
+
+    print(brightness)
     
     r = color[0]
     g = color[1]
     b = color[2]
 
-    set_brightness(brightness)
-    set_pixel(pixel, r, g, b)
+    set_pixel(pixel, r, g, b, brightness)
     show()
 
     time.sleep(interval)
